b8e541ee3e03e7d97bbf6ee8c68345338353e777
Sidhant <alairoet@gmail.com>
2024-01-07 22:52:56 +0530

    await added in verifyotp linking process

--- src/discord/listeners/modal/verifyOtp.ts
+++ src/discord/listeners/modal/verifyOtp.ts
@@ -30,7 +30,7 @@ export default async (
         });
         let roles = [];
         if (validOtp.userId) {
-            customSolution.linkDiscord(validOtp.userId, interaction.user.id);
+            await customSolution.linkDiscord(validOtp.userId, interaction.user.id);
             roles = (await fetchRoles(serviceId)) || [];
         } else {
             roles = await updateDiscordIdForPhoneNumberandFetchRoles(
